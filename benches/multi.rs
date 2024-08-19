ba1c97228d786b06fd343856cb41b3afa6539d5a
Pistonight <pistonknight@outlook.com>
2024-08-19 12:20:54 -0700

    add benches

---
+++ benches/multi.rs
@@ -0,0 +1,26 @@
+use std::time::Instant;
+
+use cooking::{Cook, CookError};
+
+fn main() -> Result<(), CookError> {
+    let start = Instant::now();
+    let cycles = 1_000_000;
+
+    let cook = Cook::new()?;
+
+    for _ in 0..cycles {
+        cook.cook(&[
+            "Mighty Carp",
+            "Roasted Acorn",
+            "Raw Bird Drumstick",
+            "Sneaky River Escargot",
+            "Yellow Lizalfos Tail"
+        ])?;
+    }
+
+    let elapsed = start.elapsed();
+    let cycles_per_second = cycles as f64 / elapsed.as_secs_f64();
+    println!("Cooked {} meals in {:.2?} ({:.2} cycles/s)", cycles, elapsed, cycles_per_second);
+
+    Ok(())
+}
